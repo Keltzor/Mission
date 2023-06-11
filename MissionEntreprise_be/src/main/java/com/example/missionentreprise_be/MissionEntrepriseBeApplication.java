0c78bce94374ca672b861015972981efa5ae08ae
Keltzor <105127342+Keltzor@users.noreply.github.com>
2023-06-11 20:25:25 +0100

    Add files via upload

---
+++ MissionEntreprise_be/src/main/java/com/example/missionentreprise_be/MissionEntrepriseBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.missionentreprise_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionEntrepriseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissionEntrepriseBeApplication.class, args);
+	}
+
+}
---
+++ MissionEntreprise_be/src/test/java/com/example/missionentreprise_be/MissionEntrepriseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.missionentreprise_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionEntrepriseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
